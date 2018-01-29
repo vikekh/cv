6acfb09f3215a9d2a209a144ebeea9db12b1d0eb
vikekh <621212+vikekh@users.noreply.github.com>
2018-01-29 22:00:57 +0100

    Playing with PhantomJS

---
+++ src/Vikekh.CV.Cli/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Vikekh.CV.PhantomJS;
+
+namespace Vikekh.CV.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phantomJS = new PhantomJSWrapper();
+            phantomJS.Execute();
+        }
+    }
+}
