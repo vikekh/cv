9454b4c1c55f25afd5d8dae465d2490fc3b30014
vikekh <621212+vikekh@users.noreply.github.com>
2018-01-29 21:10:12 +0100

    Renamed API project

--- src/Vikekh.CV.Api/Startup.cs
+++ src/Vikekh.CV.Api/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Vikekh.CV.WebApi
+namespace Vikekh.CV.Api
 {
     public class Startup
     {
