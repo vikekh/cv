147e168523272c8a1ea7e016751b9b135e738093
vikekh <621212+vikekh@users.noreply.github.com>
2020-01-14 22:17:21 +0100

    Tool for translating _data files to JSON Resume

---
+++ src/Vikekh.Cv.JsonResume.Cli/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Vikekh.Cv.JsonResume.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFiles = new string[] { "basics", "work", "volunteer", "education", "awards", "publications", "skills", "languages", "interests", "references", "projects" };
+        }
+    }
+}
