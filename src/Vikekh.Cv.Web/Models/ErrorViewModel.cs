9291bb753f94662970c3b7738dc56928193cd90e
vikekh <621212+vikekh@users.noreply.github.com>
2020-10-28 01:58:15 +0100

    ASP.NET Core MVC + Docker

---
+++ src/Vikekh.Cv.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vikekh.Cv.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
